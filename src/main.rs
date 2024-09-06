029e13f5e8f1bee4b3d1f21ed00461d4ac4d26b4
Myryk <myryk@posteo.de>
2024-09-06 09:46:06 +0200

    print + panic macros

--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,20 @@
 
 use core::panic::PanicInfo;
 
-use vga_buffer::print_somthing;
-
-static HELLO: &[u8] = b"Hello, World! THIS IS A LONGER SENTENCE";
-
 mod vga_buffer;
 
 #[no_mangle]
 //entry point to the programm
 pub extern "C" fn _start() -> ! {
-    // let vga_buffer = 0xb8000 as *mut u8;
+    // panic!("an error occured!!!!");
 
-    // for (i, &byte) in HELLO.iter().enumerate() {
-    //     unsafe {
-    //         *vga_buffer.offset(i as isize * 2) = byte;
-    //         *vga_buffer.offset(i as isize * 2 + 1) = 0x1f;
-    //     }
-    // }
-    print_somthing();
     loop {}
 }
 
 //called on panic (no unwinding)
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    
     loop {}
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,5 +1,28 @@
 use core::fmt;
-// use spin::Mutex;
+use volatile::Volatile;
+use lazy_static::lazy_static;
+use spin::Mutex;
+
+#[macro_export]
+macro_rules! print {
+	($($arg:tt)*) => {
+		$crate::vga_buffer::_print(format_args!($($arg)*));
+	};
+}
+
+#[macro_export]
+macro_rules! println {
+	($($arg:tt)*) => {
+		$crate::print!("{}\n", format_args!($($arg)*));
+	};
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments)
+{
+	use core::fmt::Write;
+	WRITER.lock().write_fmt(args).unwrap();
+}
 
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -36,8 +59,8 @@ impl ColorCode {
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(C)]
 struct ScreenChar {
-	ascii_character: u8,
-	color_code: ColorCode,
+    ascii_character: u8,
+    color_code: ColorCode,
 }
 
 const BUFFER_WIDTH: usize = 80;
@@ -45,99 +68,83 @@ const BUFFER_HEIGHT: usize = 20;
 
 #[repr(transparent)]
 struct Buffer {
-	chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
 
 pub struct Writer {
-	column_position: usize,
-	color_code: ColorCode,
-	buffer: &'static mut Buffer,
+    column_position: usize,
+    color_code: ColorCode,
+    buffer: &'static mut Buffer,
 }
 
 impl Writer {
-	pub fn write_string(&mut self, s: &str) {
-		for byte in s.bytes() {
-			match byte {
-				// printable
-				0x20..=0x7e => self.write_byte(byte),
-				// newline
-				b'\n' => self.new_line(),
-				// non-printable
-				_ => self.write_byte(0xfe),
-			}
-		}
-	}
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                // printable
+                0x20..=0x7e => self.write_byte(byte),
+                // newline
+                b'\n' => self.new_line(),
+                // non-printable
+                _ => self.write_byte(0xfe),
+            }
+        }
+    }
 
-	pub fn write_byte(&mut self, byte: u8) {
-		match byte {
-			b'\n' => self.new_line(),
-			byte => {
-				if self.column_position >= BUFFER_WIDTH {
-					self.new_line();
-				}
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
 
-				let row = BUFFER_HEIGHT -1;
-				let col = self.column_position;
+                let row = BUFFER_HEIGHT - 1;
+                let col = self.column_position;
 
-				// let color_code = self.color_code;
-				self.buffer.chars[row][col] = ScreenChar {
-					ascii_character: byte,
-					color_code: self.color_code,
-				};
-				self.column_position += 1;
-			}
-		}
-	}
+                // let color_code = self.color_code;
+                self.buffer.chars[row][col].write(ScreenChar {
+                    ascii_character: byte,
+                    color_code: self.color_code,
+                });
+                self.column_position += 1;
+            }
+        }
+    }
 
-	fn new_line(&mut self) {
-		for row in 0..BUFFER_HEIGHT - 1 {
-			for col in 0..BUFFER_WIDTH {
-				self.buffer.chars[row][col] = self.buffer.chars[row + 1][col];
-			}
-		}
-		self.clear_row(BUFFER_HEIGHT -1);
-		self.column_position = 0;
-	}
+    fn new_line(&mut self) {
+        for row in 0..BUFFER_HEIGHT - 1 {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row + 1][col].read();
+                self.buffer.chars[row][col].write(character);
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+    }
 
-	fn clear_row(&mut self, row: usize) {
-		let blank = ScreenChar {
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
             ascii_character: b' ',
             color_code: self.color_code,
         };
-		for col in 0..BUFFER_WIDTH {
-			self.buffer.chars[row][col] = blank;
-		}
-	}
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
 }
 
 impl fmt::Write for Writer {
-	fn write_str(&mut self, s: &str) -> fmt::Result {
-		self.write_string(s);
-		Ok(())
-	}
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
 }
 
-// lazy_static! {
-// 	pub static WRITER: Writer = Writer {
-// 		column_position: 0,
-// 		color_code: ColorCode::new(Color::LightGray, Color::Black),
-// 		buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-// 	};
-// }
-
-pub fn print_somthing() {
-	let mut writer = Writer {
-		column_position: 0,
-		color_code: ColorCode::new(Color::White, Color::Blue),
-		buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-	};
-
-	writer.write_byte(b'H');
-	writer.write_string(" ello is a test string! ");
-	writer.write_string("THIS IS ALL CAPS!");
-	writer.write_string("\nline one\nline two");
-	for i in 1..18 {
-		writer.write_string("newline\n");
-	}
-	writer.write_string("last_lineö");
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::LightGray, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    });
 }
