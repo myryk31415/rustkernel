3306396f8ad669d5d375f7cbe8e97242004e3541
Myryk <myryk@posteo.de>
2024-09-06 02:13:20 +0200

    implemented vga_buffer writer

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,24 @@
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello, World!";
+use vga_buffer::print_somthing;
+
+static HELLO: &[u8] = b"Hello, World! THIS IS A LONGER SENTENCE";
+
+mod vga_buffer;
 
 #[no_mangle]
 //entry point to the programm
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    // let vga_buffer = 0xb8000 as *mut u8;
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    // for (i, &byte) in HELLO.iter().enumerate() {
+    //     unsafe {
+    //         *vga_buffer.offset(i as isize * 2) = byte;
+    //         *vga_buffer.offset(i as isize * 2 + 1) = 0x1f;
+    //     }
+    // }
+    print_somthing();
     loop {}
 }
 
