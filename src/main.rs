46ee09567a1b1bb5bc4bdf20ad9e464760a3603a
Myryk <myryk@posteo.de>
2024-10-02 07:18:22 +0200

    working on paging

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,15 @@
 
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
-use rustkernel::{hlt_loop, init, println};
+use rustkernel::{hlt_loop, init, memory::active_level_four_table, println};
+use x86_64::{structures::paging::PageTable, VirtAddr};
 
 entry_point!(kernel_main);
 
 //entry point to the programm
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use rustkernel::memory::translate_addr;
+
     println!("main called!");
 
     #[cfg(test)]
@@ -19,10 +22,49 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     init();
 
-    use x86_64::registers::control::Cr3;
+    // use x86_64::registers::control::Cr3;
 
-    let (level_4_page_table, _) = Cr3::read();
-    println!("level 4 address: {:?}", level_4_page_table.start_address());
+    // let (level_4_page_table, _) = Cr3::read();
+    // println!("level 4 address: {:?}", level_4_page_table.start_address());
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    // let l4_table = unsafe { active_level_four_table(phys_mem_offset) };
+
+    let addresses = [
+        // identity-mapped vga buffer page
+        0xb8000,
+        // some code page
+        0x201008,
+        // (phys_mem_offset + 0x401008 as u64).as_u64(),cr
+        // some stack page
+        0x0100_0020_1a10,
+        // virtual address mapped to physical address 0
+        boot_info.physical_memory_offset,
+    ];
+
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        println!("{:?} -> {:?}", virt, phys);
+    }
+
+    // for (i, entry) in l4_table.iter().enumerate() {
+    //     if !entry.is_unused() {
+    //         println!("L4 Entry {}: {:?}", i, entry);
+
+    //         let phys = entry.frame().unwrap().start_address();
+    //         let virt = phys_mem_offset + phys.as_u64();
+    //         let page_table_ptr: *mut PageTable = virt.as_mut_ptr();
+
+    //         let l3_table = unsafe { &*page_table_ptr };
+
+    //         for (i, entry) in l3_table.iter().enumerate() {
+    //             if !entry.is_unused() {
+    //                 println!("L3 Entry {}: {:?}", i, entry);
+    //             }
+    //         }
+    //     }
+    // }
 
     // let ptr = 0x204013 as *mut u8;
     // unsafe { *ptr = 42 }
--- src/memory.rs
+++ src/memory.rs
@@ -1,4 +1,18 @@
-use x86_64::{structures::paging::PageTable, VirtAddr};
+use x86_64::{
+    instructions::tables,
+    structures::paging::{frame, PageTable, OffsetPageTable},
+    PhysAddr, VirtAddr,
+};
+
+/// Initialize a new OffsetPageTable
+///
+/// Complete physical memory must be mapped at offset
+/// Function can only be called once
+// pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
+//     let level_four_table
+//      = active_level_four_table(physical_memory_offset);
+//     OffsetPageTable::new(level_four_table, physical_memory_offset)
+// }
 
 /// all physical memory need to be mapped
 /// only call once
@@ -12,4 +26,42 @@ pub unsafe fn active_level_four_table(physical_memory_offset: VirtAddr) -> &'sta
     let page_table_ptr: *mut PageTable = virt.as_mut_ptr();
 
     &mut *page_table_ptr
+}
+
+/// Translates a virtual address into a physical one
+/// Returns `None` if not mapped
+///
+/// Whole physical memory needs to be mapped at given offset.
+pub unsafe fn translate_addr(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Option<PhysAddr> {
+    translate_addr_inner(addr, physical_memory_offset)
+}
+
+fn translate_addr_inner(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Option<PhysAddr> {
+    use x86_64::registers::control::Cr3;
+    use x86_64::structures::paging::page_table::FrameError;
+
+    let (level_four_table_frame, _) = Cr3::read();
+
+    let tables_indexes = [
+        addr.p4_index(),
+        addr.p3_index(),
+        addr.p2_index(),
+        addr.p1_index(),
+    ];
+    let mut frame = level_four_table_frame;
+
+    for &index in &tables_indexes {
+        let virt = physical_memory_offset + frame.start_address().as_u64();
+        let table_ptr: *mut PageTable = virt.as_mut_ptr();
+        let table = unsafe { &*table_ptr };
+
+        let entry = &table[index];
+        frame = match entry.frame() {
+            Ok(frame) => frame,
+            Err(FrameError::FrameNotPresent) => return None,
+            Err(FrameError::HugeFrame) => panic!("Hugh pages not supported"),
+        };
+    }
+
+    Some(frame.start_address() + u64::from(addr.page_offset()))
 }
